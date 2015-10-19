a2c87b6b985da5613c475abc245d264436afe0f8
zhaoxingyu <zhaoxingyu19920404@gmail.com>
2015-10-20 00:45:25 +0800

    change

--- permutationSequence.cc
+++ permutationSequence.cc
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 class Solution {
     public:
         string getPermutation(int n, int k) {
+            k -= 1;
             string res(n, '0');
             int cnt = 1;
             for (int i=0; i<n; i++) {
@@ -11,23 +13,31 @@ class Solution {
                 cnt *= (i+1);
             }
 
-            if (k<1 || k>cnt)
+            if (k<0 || k>=cnt)
                 return "";
+
             cnt /= n;
             int j;
             char tmp;
             for (int i=0; i<n-1; i++) {
-                j = k/cnt;
+                j = i + k/cnt;
                 k = k%cnt;
-                cnt = cnt/(n-i);
-                tmp = res[n-i];
-                res[n-i] = res[j];
-                for (int l=i; l<=j; l++) {
-                    res[n-i] = res[n-i-1];
-                }
-                res[j] = tmp;
+                cnt /= (n-(i+1));
+                if (j==i)
+                    continue;
+                tmp = res[i];
+                res[i] = res[j];
+                auto resBegin = res.begin();
+                copy_backward(resBegin+i+1,resBegin+j, resBegin+j+1);
+                res[i+1] = tmp;
             }
 
             return res;
         }
 };
+
+int main(void) {
+    Solution sl;
+    cout << sl.getPermutation(3, 3) << endl;
+
+}
