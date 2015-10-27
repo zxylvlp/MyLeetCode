c7cd3e1f21401a10c096e761a71170b80956deb9
zhaoxingyu <zhaoxingyu19920404@gmail.com>
2015-10-27 11:45:28 +0800

    add

--- uniqueBinarySearchTreesII.cc
+++ uniqueBinarySearchTreesII.cc
@@ -3,7 +3,31 @@ using namespace std;
 class Solution {
     public:
         vector<TreeNode*> generateTrees(int n) {
-            for ()
+            return backtrack(1,n);
+
+        }
+
+        vector<TreeNode*> backtrack(int start, int end) {
+            vector<TreeNode*> res;
+            if (start>end) {
+                res.push_back(nullptr);
+                return res;
+            }
+
+
+            for (int i=start; i<=end; i++) {
+                auto left = backtrack(start,i-1);
+                auto right = backtrack(i+1, end);
+                for (int j=0; j<left.size(); j++) {
+                    for (int k=0; k<right.size(); k++) {
+                        TreeNode* root = new TreeNode(i);
+                        root.left = left[i];
+                        root.right = right[j];
+                        res.push_back(root);
+                    }
+                }
+
+            }
 
         }
 };
